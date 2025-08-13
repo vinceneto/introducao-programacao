1ea6598c2576147d391928daae7d0d8819139d54
vinceneto <vnetoxd@gmail.com>
2025-08-13 18:43:17 -0400

    primeiro commit 2025/2

---
+++ introducao-programacao/001-entrada-saída-basicas/002-declaracao-variaveis.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    // Esse é um exemplo de comentário simples
+
+    /* 
+    Esse é um
+    exemplo de 
+    comentários múltiplos
+    */
+
+    // tipo variável <nome_da_variável>
+    // int a;  // Declarando uma variável do tipo int
+    // int b;  // Declarando outra variável do tipo int
+    // float c; // Declarando uma variável do tipo float
+
+    // a = 6;  // Armazenando o valor 6 na variável a
+    // b = 10; // Armazenando o valor 10 na variável b
+    // c = 9.9; // Armazenando o valor 9.9 na variável c
+
+    // int a, b; // Declarando duas variáveis do tipo int
+    // float c, d; // Declarando duas variáveis do tipo float
+
+    // int a = 6, b = 10;  // Declarando e Inicializando a variável a
+
+    // Nomes de variáveis válidos
+    int a; 
+    int A;
+    char nome1;
+    char nome_usuario;
+    char nomeusuario;
+    char nomeUsuarioCadastrado;
+    int _valor;
+
+    return 0;
+}
